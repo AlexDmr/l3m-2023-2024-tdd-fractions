0bd7459035a60943365905ac5ac41bbe9b4a9513
AlBob <>
2024-03-15 14:58:05 +0100

    TODO: IL faut faire un refactor pour introduire une classe Fraction, plus propre, plus conforme à une approche objet.

--- src/app/data/fractions.spec.ts
+++ src/app/data/fractions.spec.ts
@@ -31,39 +31,98 @@ describe("Instanciations de fractions possibles", () => {
         expect(f.dénominateur()).toEqual(2)
     });
 
-    it("should be possible to build the fraction 2/5 from (-2)/(-5)", () => {
+    it("should be possible to build the fraction (-2)/(-5) from (-2)/(-5)", () => {
         const f = fraction(-2, -5);
+        expect(f.numérateur()).toEqual(-2);
+        expect(f.dénominateur()).toEqual(-5)
+        expect(f.toString()).toEqual("(-2)/(-5)")
+    });
+
+    it("should be possible to build the fraction 2/(-5) from (2)/(-5)", () => {
+        const f = fraction(2, -5);
+        expect(f.numérateur()).toEqual(2);
+        expect(f.dénominateur()).toEqual(-5)
+        expect(f.toString()).toEqual("2/(-5)")
+    });
+
+    it("should be possible to build the fraction 3/(-7) from 3/(-7)", () => {
+        const f = fraction(3, -7);
+        expect(f.numérateur()).toEqual(3);
+        expect(f.dénominateur()).toEqual(-7)
+        expect(f.toString()).toEqual("3/(-7)")
+    });
+
+    it("should be possible to build the fraction (-5)/(-1) from (-5)/(-1), should serialized to '(-5)/(-1)'", () => {
+        const f = fraction(-5, -1);
+        expect(f.numérateur()).toEqual(-5);
+        expect(f.dénominateur()).toEqual(-1)
+        expect(f.toString()).toEqual("(-5)/(-1)")
+    });
+
+    it("should be possible to build the fraction 12/(-9) from (12)/(-9)", () => {
+        const f = fraction(12, -9);
+        expect(f.numérateur()).toEqual(12);
+        expect(f.dénominateur()).toEqual(-9)
+        expect(f.toString()).toEqual("12/(-9)")
+    });
+
+    it("should be possible to build the fraction (-3)/2", () => {
+        const f = fraction(-3, 2);
+        expect(f.numérateur()).toEqual(-3);
+        expect(f.dénominateur()).toEqual(2)
+        expect(f.toString()).toEqual("(-3)/2")
+    });
+
+    it("should be possible to build the fraction 5/1", () => {
+        const f = fraction(5, 1);
+        expect(f.numérateur()).toEqual(5);
+        expect(f.dénominateur()).toEqual(1)
+        expect(f.toString()).toEqual("5/1")
+    });
+
+});
+
+describe("Simplification de fractions", () => {
+    it("should be possible to simplify (-2)/(-5) into 2/5", () => {
+        const f = fraction(-2, -5).simplify();
         expect(f.numérateur()).toEqual(2);
         expect(f.dénominateur()).toEqual(5)
         expect(f.toString()).toEqual("2/5")
     });
 
-    it("should be possible to build the fraction -2/5 from (2)/(-5)", () => {
-        const f = fraction(2, -5);
+    it("should be possible to simplify (-2)/5 into (-2)/5", () => {
+        const f = fraction(-2, 5).simplify();
         expect(f.numérateur()).toEqual(-2);
         expect(f.dénominateur()).toEqual(5)
-        expect(f.toString()).toEqual("-2/5")
+        expect(f.toString()).toEqual("(-2)/5")
     });
 
-    it("shouldn't be possible to build the fraction -3/7 from 3/(-7)", () => {
-        const f = fraction(3, -7);
+    it("should be possible to simplify 3/(-7) into (-3)/7", () => {
+        const f = fraction(3, -7).simplify();
         expect(f.numérateur()).toEqual(-3);
         expect(f.dénominateur()).toEqual(7)
-        expect(f.toString()).toEqual("-3/7")
+        expect(f.toString()).toEqual("(-3)/7")
     });
 
-    it("shouldn't be possible to build the fraction 5 from (-5)/(-1)", () => {
-        const f = fraction(-5, -1);
-        expect(f.numérateur()).toEqual(5);
-        expect(f.dénominateur()).toEqual(1)
-        expect(f.toString()).toEqual("5")
-    });
-
-    it("shouldn't be possible to build the fraction -4/3 from (12)/(-9)", () => {
-        const f = fraction(12, -9);
+    it("should be possible to simplify 256/(-64) into -4", () => {
+        const f = fraction(256, -64).simplify();
         expect(f.numérateur()).toEqual(-4);
-        expect(f.dénominateur()).toEqual(3)
-        expect(f.toString()).toEqual("-4/3")
+        expect(f.dénominateur()).toEqual(1)
+        expect(f.toString()).toEqual("-4")
     });
 
+})
+
+
+describe("Multiplication de fractions", () => {
+    it("should be possible to multiply 3/2 by 4/5 and get 12/10", () => {
+        const f32 = fraction(3, 2);
+        const f45 = fraction(4, 5);
+        const f65 = f32.multiply(f45);
+        expect(f65.numérateur()).toEqual(12)
+        expect(f65.dénominateur()).toEqual(10)
+        
+        expect(f32.toString()).withContext("La fraction sur laquelle on appelle la multiplication reste inchangée").toEqual("3/2")
+        expect(f45.toString()).withContext("Le paramètre de la multiplication rete inchangé").toEqual("4/5")
+    });
 });
--- src/app/data/fractions.ts
+++ src/app/data/fractions.ts
@@ -9,14 +9,29 @@ export interface Fraction {
     toString(): string;
 
     /**
+     * Renvoie la forme simplifié de la fraction.
+     * La forme irréductible.
+     */
+    simplify(): Fraction;
+
+    /**
      * Accesseur pour obtenir le nominateur
      */
     numérateur(): number;
-    
+
     /**
      * Accesseur pour obtenir le dénominateur
      */
     dénominateur(): number;
+
+    /**
+     * Multiplie la fraction par f.
+     * Renvoie le résultat sous forme simplifié.
+     * Renvoie une nouvelle fraction, résultat de cette multiplication
+     * La fraction reste inchangée.
+     * @param f La fraction avec laquelle on va multiplier. Reste inchangée.
+     */
+    multiply(f: Fraction): Fraction;
 }
 
 /**
@@ -33,19 +48,59 @@ export const illFormedException = new Error("ILL FORMED fraction, should be of t
  * @returns La fraction nominateur / dénominateur si les contraintes sont respectées
  * @throws "ILL FORMED fraction, should be of the form Z/Z"
  */
-export function fraction(numérateur: number, dénominateur: number): Fraction {
-    if ( !Number.isInteger(numérateur)
+export function fraction(numérateur: number, dénominateur: number, simplified = false): Fraction {
+    if (!Number.isInteger(numérateur)
         || !Number.isInteger(dénominateur)
         || dénominateur === 0
     ) throw illFormedException;
 
-    const s = Math.sign(numérateur) * Math.sign(dénominateur)
-    const n = s * Math.abs(numérateur);
-    const d = Math.abs(dénominateur);
+    const n = numérateur
+    const d = dénominateur
+
+    // Sérialise un entier Z, met les parenthèses si négatif
+    function seriN(n: number): string {
+        return n >= 0 ? `${n}` : `(${n})`
+    }
+    let str = (simplified && d === 1) ? `${n}` : `${seriN(n)}/${seriN(d)}`
 
     return {
-        toString:     () => `${n}/${d}`,
-        numérateur:   () => n,
-        dénominateur: () => d
+        toString: () => str,
+        numérateur: () => numérateur,
+        dénominateur: () => dénominateur,
+        multiply: (f: Fraction) => fraction(n * f.numérateur(), d * f.dénominateur()),
+        simplify: () => {
+            const s = Math.sign(numérateur) * Math.sign(dénominateur)
+            const N = Math.abs(numérateur);
+            const D = Math.abs(dénominateur);
+
+            const pgcd = euclide(N, D);
+
+            const n = N / pgcd
+            const d = D / pgcd
+            
+            return fraction(s*n, d, true)
+        }
     }
+}
+
+
+
+/**
+ * Calcule le PGCD de deux entiers positifs
+ * A compléter pour prendre en charge les nombres non entier positifs.
+ * @param a Un entier positif
+ * @param b Un entier positif
+ * @returns Le PGCD de a et b
+ */
+function euclide(a: number, b: number) {
+
+    while (a !== b) {
+        if (a > b) {
+            a = a - b;
+        } else {
+            b = b - a;
+        }
+    }
+
+    return a
 }
